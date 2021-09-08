86353ae0b7df3ce1ebfc191898ece6d523933a37
aleksq7 <morrisalex2001@gmail.com>
2021-09-08 12:02:01 -0600

    commit tree 76fba4678077d6e9f18dc58c841377729919dc74

--- index.js
+++ index.js
@@ -1,4 +1,76 @@
 /* Your Code Here */
+function createEmployeeRecord([name1, name2, title, payPerHour]){
+    let newRecord = {
+        firstName: name1,
+        familyName: name2,
+        title: title,
+        payPerHour: payPerHour,
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return newRecord;
+}
+
+function createEmployeeRecords(arrays){
+    return arrays.map(array => createEmployeeRecord(array));
+}
+
+function createTimeInEvent(dateStr/*MUST BE IN 'YYYY-MM-DD HHMM' FORMAT */){
+    let timeInEvent = {
+        type: 'TimeIn',
+        hour: parseInt(dateStr.slice(11)),
+        date: dateStr.slice(0,10)
+    }
+    this.timeInEvents.push(timeInEvent)
+    return this;
+}
+
+function createTimeOutEvent(dateStr/*MUST BE IN 'YYYY-MM-DD HHMM' FORMAT */){
+    let timeOutEvent = {
+        type: 'TimeOut',
+        hour: parseInt(dateStr.slice(11)),
+        date: dateStr.slice(0,10)
+    }
+    this.timeOutEvents.push(timeOutEvent)
+    return this;
+}
+
+ function hoursWorkedOnDate(dateStr/*'YYYY-MM-DD' FORMAT HERE, NO HOUR*/){
+    let a = 0;
+    let b = 0;
+    this.timeInEvents.forEach(event => {
+        if (event.date === dateStr){
+            a = event.hour;
+        }
+    })
+    this.timeOutEvents.forEach(event =>{
+        if (event.date === dateStr){
+            b = event.hour;
+        }
+    })
+    return (b-a)/100;
+}
+
+function wagesEarnedOnDate(dateStr/*YYYY-MM-DD*/){
+    return this.payPerHour * hoursWorkedOnDate.call(this, dateStr);
+}
+
+function findEmployeeByFirstName(srcArray, firstName){
+    let result;
+    srcArray.forEach(record => {
+        if (record.firstName === firstName){
+            result = record;
+        }
+    })
+    return result;
+}
+
+function calculatePayroll(srcArray){
+    let result = 0;
+    srcArray.forEach(record => result += allWagesFor.call(record))
+    return result;
+}
+
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
